965f2a4ffaf1391fba7416a642411546442e0675
Florent Uguet <florent.uguet@actemium.com>
2018-09-27 09:22:38 +0200

    Cleared Package list

--- FHXTools/Data/PV AI PID.cs
+++ FHXTools/Data/PV AI PID.cs
@@ -3,7 +3,7 @@ using FHXTools.FHX;
 bool Validate(FHXParameter p)
 {
     //Recherche les PV des AI et des PID
-    if (p.Parent.Name == "AI1/PV" || p.parent.Name == "PID1/PV" )
+    if (p.Parent.Name == "AI1/PV" || p.Parent.Name == "PID1/PV" )
     {
         //Récupère le CV
         if (p.Name == "CV")
